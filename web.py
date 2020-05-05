b20e8a0858f60b21bf979358ebc5558800f1c93e
Raniac <leibingye@outlook.com>
2020-05-05 20:22:34 +0800

    Added virtualenv.

--- web.py
+++ web.py
@@ -5,9 +5,19 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world!")
 
+class SearchHandler(tornado.web.RequestHandler):
+    def get(self):
+        pass
+
+class AddHandler(tornado.web.RequestHandler):
+    def post(self):
+        pass
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/search/(*)", SearchHandler),
+        (r"/add/", AddHandler),
     ])
 
 if __name__ == "__main__":
