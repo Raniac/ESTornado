a5fc6e373536fbc5137673164fa208efe0618f4c
Raniac <leibingye@outlook.com>
2020-05-05 20:51:30 +0800

    Deployed elasticsearch.

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # ElasticSearch with Tornado
 
+## Start Web Server
+
 ```bash
-source env/bin/activate
-python3 web.py
+# source env/bin/activate
+# python3 web.py
+./start.sh
+```
+
+## Initiate ElasticSearch Container
+
+```bash
+docker run -it --rm -e "discovery.type=single-node" -p 9200:9200 -p 9300:9300 elasticsearch:7.4.2
+```
+
+## ElasticSearch Hands-On - Python API
+
+```python
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host':'127.0.0.1','port':9200}])
+es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
 ```
--- web.py
+++ web.py
@@ -1,23 +1,29 @@
 import tornado.ioloop
 import tornado.web
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host':'127.0.0.1','port':9200}])
+es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world!")
 
 class SearchHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        pass
+
     def get(self):
         pass
 
-class AddHandler(tornado.web.RequestHandler):
-    def post(self):
-        pass
+# class AddHandler(tornado.web.RequestHandler):
+#     def post(self):
+#         pass
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/search/(*)", SearchHandler),
-        (r"/add/", AddHandler),
+        # (r"/search/(*)", SearchHandler),
+        # (r"/add/", AddHandler),
     ])
 
 if __name__ == "__main__":
