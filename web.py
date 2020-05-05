98c6f6033a5204101eb74f1dbd3638be6b8d1ff6
Raniac <leibingye@outlook.com>
2020-05-05 21:39:45 +0800

    Added interfaces for search and add.

--- web.py
+++ web.py
@@ -1,29 +1,41 @@
 import tornado.ioloop
 import tornado.web
+import tornado.gen
 from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host':'127.0.0.1','port':9200}])
-es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world!")
 
 class SearchHandler(tornado.web.RequestHandler):
-    def initialize(self):
-        pass
+    def initialize(self, es): 
+        self.es = es
 
     def get(self):
-        pass
+        q = self.get_query_argument('q', '')
+        try:
+            r = self.es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
+        except Exception as e:
+            r = str(e)
+        response = {'query': q, 'results': r}
+        self.write(response)
 
-# class AddHandler(tornado.web.RequestHandler):
-#     def post(self):
-#         pass
+class AddHandler(tornado.web.RequestHandler):
+    def initialize(self, es): 
+        self.es = es
+
+    def post(self):
+        data = json.loads(self.request.body)
+        response = {'query': data['q']}
+        self.write(response)
 
 def make_app():
+    es = Elasticsearch([{'host':'127.0.0.1','port':9200}])
     return tornado.web.Application([
         (r"/", MainHandler),
-        # (r"/search/(*)", SearchHandler),
-        # (r"/add/", AddHandler),
+        (r"/search", SearchHandler, dict(es=es)),
+        (r"/add", AddHandler, dict(es=es)),
     ])
 
 if __name__ == "__main__":
