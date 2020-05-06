6d9927e5483e92aea85b5f4f1367a3e371e8cbf0
Raniac <leibingye@outlook.com>
2020-05-06 17:14:54 +0800

    Tested async non-blocking.

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import tornado.web
 import tornado.gen
 from elasticsearch import Elasticsearch
 import json
+import time
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -12,14 +13,50 @@ class SearchHandler(tornado.web.RequestHandler):
     def initialize(self): 
         self.es = Elasticsearch([{'host':'127.0.0.1', 'port':9200}])
 
-    def get(self):
+    async def get(self):
         q = self.get_query_argument('q', '')
         try:
-            r = self.es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
+            # r = await self.es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
+            r = await IOTest()
         except Exception as e:
             r = str(e)
+            print(e)
         response = {'query': q, 'results': r}
         self.write(response)
+    
+    # def get(self):
+    #     q = self.get_query_argument('q', '')
+    #     try:
+    #         # r = self.es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
+    #         r = IOTest()
+    #     except Exception as e:
+    #         r = str(e)
+    #         print(e)
+    #     response = {'query': q, 'results': r}
+    #     self.write(response)
+    
+    # @tornado.gen.coroutine
+    # def get(self):
+    #     q = self.get_query_argument('q', '')
+    #     try:
+    #         # r = self.es.search(index='logstash-2015.08.20', q='http_status_code:5* AND server_name:"web1"', from_='124119')
+    #         r = yield IOTest()
+    #     except Exception as e:
+    #         r = str(e)
+    #         print(e)
+    #     response = {'query': q, 'results': r}
+    #     self.write(response)
+
+async def IOTest():
+    await tornado.gen.sleep(10)
+    return 'Non-Blocking'
+# def IOTest():
+#     time.sleep(10)
+#     return 'Blocking'
+# @tornado.gen.coroutine
+# def IOTest():
+#     yield tornado.gen.sleep(10)
+#     raise tornado.gen.Return('Non-Blocking')
 
 class AddHandler(tornado.web.RequestHandler):
     def initialize(self): 
